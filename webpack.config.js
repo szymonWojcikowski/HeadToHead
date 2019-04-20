86714e623ce3fdc4124463ac11f736876f2dc7d9
Szymon <szymon.wojcikowski@prokonto.pl>
2019-04-20 21:25:08 +0200

    corrected publicPath in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, ""),
-        publicPath: "/"
+        publicPath: ""
     },
     devServer: {
         inline: true,
